35097e01eed99318bd0db1a35aec54783b4f0ddc
Jeke Zulu <jekezulu28@gmail.com>
2026-01-14 14:44:28 +0200

    new

--- enrollment/urls.py
+++ enrollment/urls.py
@@ -5,7 +5,7 @@ app_name = 'enrollment'  # in enrollment/urls.py
 
 urlpatterns = [
     path('', views.enrollment_info, name='enrollment_info'),
-    path('apply/', views.enrollment_apply, name='enrollment_apply'),
+    path('apply/', views.enrollment_apply, name='apply'),
     path('apply/success/<uuid:application_id>/', views.enrollment_success, name='enrollment_success'),
     path('check-status/', views.check_status, name='check_status'),
 ]
