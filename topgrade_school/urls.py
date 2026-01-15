50c6b63ac56c17586486e94a57022da6a5d9dfe7
Jeke Zulu <jekezulu28@gmail.com>
2026-01-15 10:04:00 +0200

    my

--- topgrade_school/urls.py
+++ topgrade_school/urls.py
@@ -1,8 +1,5 @@
 """
 URL configuration for topgrade_school project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
 """
 from django.contrib import admin
 from django.urls import path, include
